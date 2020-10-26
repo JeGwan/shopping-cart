cb35c41bac79c5f1db3dd49e16e28d1924cf5108
JeGwan <emperorvv@gmail.com>
2020-10-26 16:32:59 +0900

    products 페이지 pagination 적용

---
+++ components/ProductList.tsx
@@ -0,0 +1,132 @@
+import { HeartFilled, PlusOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { HTMLAttributes } from "react";
+import styled from "styled-components";
+import { ProductItem } from "../lib/types";
+import Row from "./Row";
+
+interface ProductItemCompProps extends HTMLAttributes<HTMLUListElement> {
+  productItems: ProductItem[];
+}
+const ProdcutListContainer = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+const ProductItemContainer = styled.li`
+  display: inline-block;
+  list-style: none;
+  line-height: 1.6;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  margin: 0 0 6px 6px;
+  width: 288px;
+  &:nth-child(3n + 1) {
+    margin-left: 0;
+  }
+  @media (max-width: 900px) {
+    width: 100%;
+    margin: 0 0 6px 0;
+  }
+  &:hover {
+    box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.1);
+    .thumbnail {
+      img {
+        transform: scale(1.1);
+      }
+    }
+  }
+  .thumbnail {
+    height: 150px;
+    overflow: hidden;
+    border-radius: 4px 4px 0 0;
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+      transition: all 0.3s;
+    }
+    @media (max-width: 900px) {
+      height: 200px;
+    }
+  }
+  .content {
+    padding: 8px;
+    .title {
+      height: 50px;
+      margin: 0;
+      font-size: 16px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    .score {
+      margin-left: auto;
+      font-size: 11px;
+      line-height: 16px;
+      display: flex;
+      align-items: center;
+      color: rgb(133, 138, 141);
+    }
+    .price {
+      font-weight: bold;
+      margin-right: auto;
+    }
+  }
+`;
+const Coupon = styled.div<{ available: boolean }>`
+  height: 20px;
+  border-radius: 3px;
+  padding-left: 6px;
+  padding-right: 6px;
+  background-color: rgb(248, 248, 249);
+  flex: 0 0 auto;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 9px;
+  line-height: 12px;
+  color: ${({ available }) =>
+    available ? "rgb(133, 138, 141)" : "rgb(243, 51, 64)"};
+  font-weight: bold;
+`;
+const ProductList = ({ productItems, ...props }: ProductItemCompProps) => {
+  return (
+    <ProdcutListContainer {...props}>
+      {productItems.map((productItem, index) => {
+        const couponAvailable = productItem.availableCoupon !== false;
+        return (
+          <ProductItemContainer key={index}>
+            <div className="thumbnail">
+              <img src={productItem.coverImage} />
+            </div>
+            <div className="content">
+              <Row>
+                <Coupon available={couponAvailable}>
+                  {couponAvailable ? "쿠폰 가능" : "쿠폰 불가"}
+                </Coupon>
+                <div className="score">
+                  <HeartFilled translate="no" style={{ marginRight: 2 }} />
+                  {productItem.score.toLocaleString("en-US")}
+                </div>
+              </Row>
+              <h3 className="title">{productItem.title}</h3>
+              <Row>
+                <div className="price">
+                  {productItem.price.toLocaleString("en-US")}원
+                </div>
+                <Button
+                  type="primary"
+                  size="small"
+                  icon={<PlusOutlined translate="no" />}
+                >
+                  담기
+                </Button>
+              </Row>
+            </div>
+          </ProductItemContainer>
+        );
+      })}
+    </ProdcutListContainer>
+  );
+};
+
+export default ProductList;
---
+++ pages/products.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { Button, Input, Pagination, Select } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import Layout from "../components/Layout";
+import Api from "../lib/api";
+import { mergeQueryString, Queries, usePagination } from "../lib/utils";
+import ProductList from "../components/ProductList";
+import Row from "../components/Row";
+const { Option } = Select;
+const ProductsPage = () => {
+  const router = useRouter();
+  const { page, search, sort, itemsPerPage, order } = usePagination();
+  const [searchInput, setSearchInput] = useState(search);
+  const response = Api.getProducts({
+    currentPage: page,
+    search,
+    sort,
+    itemsPerPage,
+    order,
+  });
+  const move = (queries: Queries) => {
+    router.push(mergeQueryString(router, queries));
+  };
+  if (!response.success) {
+    console.error(`💩 error from server : `, response.message);
+    return (
+      <Layout title="상품 보기">
+        오류가 발생했어요. 잠시 후 다시 시도해주세요!
+      </Layout>
+    );
+  }
+  const { currentPage, productItems, total } = response.data;
+  return (
+    <Layout title="상품 보기">
+      <Row style={{ marginBottom: 6 }}>
+        <Select
+          style={{ marginRight: 6 }}
+          value={sort}
+          onChange={(sort) => move({ page: 1, sort })}
+        >
+          <Option value="score">점수별</Option>
+          <Option value="price">가격별</Option>
+        </Select>
+        <Select value={order} onChange={(order) => move({ page: 1, order })}>
+          <Option value="desc">내림차순</Option>
+          <Option value="asc">오름차순</Option>
+        </Select>
+        <form
+          style={{ display: "flex", marginLeft: "auto" }}
+          onSubmit={(e) => {
+            e.preventDefault();
+            move({ page: 1, search: searchInput });
+          }}
+        >
+          <Input
+            placeholder="검색"
+            style={{ width: 150, marginRight: 2 }}
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <Button
+            type="primary"
+            htmlType="submit"
+            icon={<SearchOutlined translate="no" />}
+          />
+        </form>
+      </Row>
+      <ProductList productItems={productItems} />
+      <Row>
+        <Pagination
+          style={{ margin: "0 8px 0 auto" }}
+          defaultCurrent={currentPage}
+          total={total}
+          pageSize={itemsPerPage}
+          onChange={(nextPage) => move({ page: nextPage })}
+        />
+        <Select
+          value={itemsPerPage}
+          onChange={(itemsPerPage) => move({ page: 1, itemsPerPage })}
+        >
+          <Option value={3}>3개 / 페이지</Option>
+          <Option value={6}>6개 / 페이지</Option>
+          <Option value={12}>12개 / 페이지</Option>
+        </Select>
+      </Row>
+    </Layout>
+  );
+};
+
+export default ProductsPage;
